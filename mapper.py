f6dac3e8b19deb101cdafdf43a2e0c99c022a8fd
xienaiwen <sienaiwun@gmail.com>
2016-12-29 13:10:18 +0800

    'update'

--- mapper.py
+++ mapper.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python
-
-import sys
-
-count = 0
-# input comes from STDIN (standard input)
-for line in sys.stdin:
+# -*- coding: utf-8 -*-  
+import sys  
+count =0
+# input comes from STDIN (standard input)  
+for line in sys.stdin: 
+    #print line.decode('UTF-8')
+    # remove leading and trailing whitespace  
     count += 1
     if count == 1:
         continue
     # remove leading and trailing whitespace
     line = line.strip()
-    print 'count:%s----------------------------------->>' % count
-    print 'line:----->>', line
+    #print 'count:%s----------------------------------->>' % count
+    #print 'line:----->>', line
     # split the line into words
     words = line.split(',')
-    print 'words:%s--------->>len:%s' % (words, len(words))
+    #print 'words:%s--------->>len:%s' % (words, len(words))
     if not len(words) > 1:
         continue
     # increase counters
+    '''
     num = 0
     for word in words:
+        word = word.decode('UTF-8')
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
@@ -30,3 +32,10 @@ for line in sys.stdin:
             print '%s\t%s' % (word, 1)
 	    continue
         num +=1
+    '''
+    words = line.split(',') 
+    user_id = words[1]
+    if user_id:
+        print '%s\t%s' % (user_id, 1)  
+
+    
